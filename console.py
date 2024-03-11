76c1c63d20c1cd95d1e47dd8c1f1c742156a52e3
melon <affabmelon@gmail.com>
2024-03-11 05:37:27 -0700

    m

--- console.py
+++ console.py
@@ -1,21 +1,38 @@
+#!/usr/bin/python3
 import cmd
-import shlex
+from shlex import split
 from models.base_model import BaseModel
-
 
 
 class HBNBCommand(cmd.Cmd):
     '''AirBnb Command Processor'''
 
     intro = "CLI for AirBnB app"
-    prompt = "Enter a comand:"
-    
+    prompt = "(hbnb)"
+
+    def emptyline(self):
+        """Do nothing for no command pass"""
+        pass
+
+    def do_create(self,arg):
+        """Usage: create <class>
+        create a new class and print its id
+        """
+        argument_p = parse(arg)
+        if len(argument_p):
+            if argument_p[0] not in HBNBCommand.__classes:
+                print ("** class doesn't exist **")
+            else:
+                print(eval(argument_p[0]).id)
+                storage.save()
+
     def do_quit(self, line):
         '''Quit command to exit program '''
         return (True)
 
     def do_EOF(self, line):
         '''End of file'''
+        print("")
         return (True)
 
 
