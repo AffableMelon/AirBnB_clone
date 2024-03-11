020b58629d62a7a2d668b3e25750ac29b3eda6fc
melon <affabmelon@gmail.com>
2024-03-11 05:07:21 -0700

    m

--- console.py
+++ console.py
@@ -9,22 +9,7 @@ class HBNBCommand(cmd.Cmd):
 
     intro = "CLI for AirBnB app"
     prompt = "Enter a comand:"
-
-    def do_create(self, line):
-        '''
-        create a new instance of BaseModel
-        save to json file
-        print id
-        '''
-        class_name = line.shlex.split()[0]
-        if not line:
-            print("*** class name missing ***")
-        elif class_name not in self.classes.keys():
-            print (" *** class doesn't exist ***")
-        
-
-
-
+    
     def do_quit(self, line):
         '''Quit command to exit program '''
         return (True)
